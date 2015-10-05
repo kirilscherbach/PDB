62c0793374eaeb99024416997e39039d2c2f9340
kirilscherbach <kiril.scherbach@gmail.com>
2015-10-05 18:04:01 +0300

    Update sales_fact_source_to_stg_to_t.sql

--- sales_fact_source_to_stg_to_t.sql
+++ sales_fact_source_to_stg_to_t.sql
@@ -148,26 +148,47 @@ insert into public.xrte_daily (select xrte_date, currency_code, xrte_value from 
 insert into xrte_ranked (select rank() over (partition by currency_code order by xrte_date) as pk, xrte_date, currency_code, xrte_value from public.xrte_daily where xrte_date>'2014-01-01' and currency_code='BYR');
 
 
-with base as 
-(select 
-x1.xrte_date
-, x1.currency_code
-, x1.xrte_value
-, (x1.xrte_value+x2.xrte_value+x3.xrte_value)/3 as avg_xrte_value 
-	from xrte_ranked x1 	
-	inner join xrte_ranked x2 on x1.pk=x2.pk+1 and x1.currency_code=x2.currency_code 
-	inner join xrte_ranked x3 on x1.pk=x3.pk-1 and x1.currency_code=x3.currency_code 
-where x1.xrte_date between '2013-01-01' and '2015-12-31' and (DATE_PART('day',x1.xrte_date)=7 or DATE_PART('day',x1.xrte_date)=21)
-)
+WITH base AS
+  (SELECT x1.xrte_date ,
+          x1.currency_code ,
+          x1.xrte_value ,
+          (x1.xrte_value+x2.xrte_value+x3.xrte_value)/3 AS avg_xrte_value
+   FROM xrte_ranked x1
+   INNER JOIN xrte_ranked x2 ON x1.pk=x2.pk+1
+   AND x1.currency_code=x2.currency_code
+   INNER JOIN xrte_ranked x3 ON x1.pk=x3.pk-1
+   AND x1.currency_code=x3.currency_code
+   WHERE x1.xrte_date BETWEEN '2013-01-01' AND '2015-12-31'
+     AND (DATE_PART('day',x1.xrte_date)=7
+          OR DATE_PART('day',x1.xrte_date)=21)),
+     ym_xrte AS
+  (SELECT date_part('month', CASE WHEN date_part('day', x1.xrte_date)<7
+                    AND date_part('month', x1.xrte_date)>1 THEN to_date(date_part('day', x1.xrte_date)||'.'||(date_part('month', x1.xrte_date)-1)||'.'||date_part('year', x1.xrte_date), 'DD.MM.YYYY') WHEN date_part('day', x1.xrte_date)<7
+                    AND date_part('month', x1.xrte_date)=1 THEN to_date(date_part('day', x1.xrte_date)||'.'||12||'.'||(date_part('year', x1.xrte_date)-1), 'DD.MM.YYYY') ELSE x1.xrte_date END) AS xrte_month,
+          date_part('year', CASE WHEN date_part('day', x1.xrte_date)<7
+                    AND date_part('month', x1.xrte_date)>1 THEN to_date(date_part('day', x1.xrte_date)||'.'||(date_part('month', x1.xrte_date)-1)||'.'||date_part('year', x1.xrte_date), 'DD.MM.YYYY') WHEN date_part('day', x1.xrte_date)<7
+                    AND date_part('month', x1.xrte_date)=1 THEN to_date(date_part('day', x1.xrte_date)||'.'||12||'.'||(date_part('year', x1.xrte_date)-1), 'DD.MM.YYYY') ELSE x1.xrte_date END) AS xrte_year,
+          x1.currency_code ,
+          x1.xrte_value
+   FROM xrte_ranked x1),
+     ma_xrte AS
+  (SELECT DISTINCT xrte_year,
+                   xrte_month,
+                   min(xrte_value) over (partition BY xrte_year, xrte_month) AS min_xrte_value,
+                                   avg(xrte_value) over (partition BY xrte_year, xrte_month) AS avg_xrte_value
+   FROM ym_xrte)
 
-select 	b1.xrte_date||' vs '||b2.xrte_date as caption
-	, b1.xrte_value as payday_xrte
-	, b2.xrte_value as advance_xrte
-	, b1.xrte_value>b2.xrte_value -- check if xrte_rate on payday is higher then on advance day
-	, b1.avg_xrte_value>b2.avg_xrte_value
-	from 
-	base b1 inner join base b2 
-		on date_part('year', b1.xrte_date)=date_part('year', b2.xrte_date) 
-		and date_part('month', b1.xrte_date)=date_part('month', b2.xrte_date)
-		and date_part('day', b1.xrte_date)=7 and date_part('day', b2.xrte_date)=21;
 
+SELECT b1.xrte_date||' vs '||b2.xrte_date AS caption ,
+                             round(b1.xrte_value) AS payday_xrte ,
+                             round(b2.xrte_value) AS advance_xrte ,
+                             round(b3.min_xrte_value) as minimal_value,
+                             round(b3.avg_xrte_value) as avg_xrte_value
+FROM base b1
+INNER JOIN base b2 ON date_part('year', b1.xrte_date)=date_part('year', b2.xrte_date)
+AND date_part('month', b1.xrte_date)=date_part('month', b2.xrte_date)
+AND date_part('day', b1.xrte_date)=7
+AND date_part('day', b2.xrte_date)=21
+INNER JOIN ma_xrte b3 on date_part('year', b1.xrte_date)=b3.xrte_year
+AND date_part('month', b1.xrte_date)=b3.xrte_month
+order by 1;
